96cabfa411da42ac4598d088a40da06d9b5a2482
Ricardo Flores <rik.fm1809@gmail.com>
2020-07-14 14:36:22 -0500

    docs agregado

---
+++ docs/precache-manifest.ad1c0dcf474439e7b1487319c433838a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca6b5801fcba6701a08a9a2d7e4a6815",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccc22b4add5b9041a346",
+    "url": "/static/css/main.f89a3843.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccc22b4add5b9041a346",
+    "url": "/static/js/main.6278d03a.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
